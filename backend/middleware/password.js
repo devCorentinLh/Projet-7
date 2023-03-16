eb96a35d82dc658a0d8a42b0919b1ed6cbc0a220
CorentinLh <120260690+devCorentinLh@users.noreply.github.com>
2023-03-16 17:04:10 +0100

    Update password.js

--- backend/middleware/password.js
+++ backend/middleware/password.js
@@ -9,7 +9,7 @@ passwordSchema
 .has().lowercase()
 .has().digits(1)
 .has().not().spaces()
-.is().not().oneOf(["Passw0rd", "Password123"]);
+.is().not().oneOf(["Passw0rd123", "P@ssword123"]);
 
 module.exports = (req, res, next) => {
     if(passwordSchema.validate(req.body.password)) {
