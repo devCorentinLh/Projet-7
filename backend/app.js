99aeb7f76d9fe4b776e3fb5bbd4830827198b40e
CorentinLh <120260690+devCorentinLh@users.noreply.github.com>
2023-03-20 19:02:30 +0100

    mise en place du .env

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,38 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require('path');
 const rateLimit = require("express-rate-limit");
+
+const dotEnv = require("dotenv").config();
+
+const mongoRoute = (process.env.MONGO);
+
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
-const dotEnv = require("dotenv")
-dotEnv.config()
-const mongoRoute = process.env.MONGO_ROUTE;
-// Utilisation des variables d'environnement
-const limiterDatas = rateLimit({ windowMs: 15 * 60 * 1000, max: 100, message: "Trop de requêtes API lancées (> 100) dans les 15 dernières minutes."});
 
+// Limites les requêtes
+const limiterDatas = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 300,
+  message: "Trop de requêtes API lancées (300 dans les 15 dernières minutes)."
+});
+
+// Connexion Mongo via .env
 mongoose.connect(mongoRoute,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée!'));
+  .then(() => console.log('Connexion Mongo réussie'))
+  .catch(() => console.log('Connexion Mongo échouée'));
 
 const app = express();
 
-// Requetes CORS
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization" );
-  res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS" );
-  next();});
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 
 app.use(express.json());
 app.use("/api/books", limiterDatas, stuffRoutes);
